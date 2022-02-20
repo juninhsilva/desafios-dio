0ee097c9637ebbedea356b0ab763466880135c28
Gilmar Pereira da Silva Júnior <juninhsilva@live.com>
2022-02-19 23:28:06 -0300

    desafio c#

---
+++ C#/Introdução a Resolução de Desafios com C#/NumerosImpares/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i += 2)
+        {
+            if (i % 2 == 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
